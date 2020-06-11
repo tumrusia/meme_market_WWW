3ff5e99fb69bd93d795ffb6a6bcd9ba47915a14b
tumrusia <marta.markocka99@gmail.com>
2020-06-11 20:15:58 +0200

    csurf works

--- serwer.ts
+++ serwer.ts
@@ -1,11 +1,8 @@
-import {createServer} from 'http';
 import * as fs from 'fs';
 import {promisify} from 'util';
 import * as sqlite3 from 'sqlite3';
-import { Interface } from 'readline';
 
 let open = promisify(fs.open);
-let write = promisify(fs.write);
 let close = promisify(fs.close);
 sqlite3.verbose();
 let db = new sqlite3.Database('meme.db');
@@ -16,33 +13,33 @@ function addMemeToDB(filename: string, price: number) {
 }
 
 function add10Memes() {
-    addMemeToDB('./assets/1.jpg', 10);
-    addMemeToDB('./assets/2.jpg', 11);
-    addMemeToDB('./assets/3.jpg', 12);
-    addMemeToDB('./assets/4.jpg', 13);
-    addMemeToDB('./assets/5.jpg', 14);
-    addMemeToDB('./assets/6.jpg', 15);
-    addMemeToDB('./assets/7.jpg', 16);
-    addMemeToDB('./assets/8.jpg', 17);
-    addMemeToDB('./assets/9.jpg', 18);
-    addMemeToDB('./assets/10.jpg', 19);
+    addMemeToDB('1.jpg', 20);
+    addMemeToDB('2.jpg', 21);
+    addMemeToDB('3.jpg', 22);
+    addMemeToDB('4.jpg', 23);
+    addMemeToDB('5.jpg', 24);
+    addMemeToDB('6.jpg', 25);
+    addMemeToDB('7.jpg', 26);
+    addMemeToDB('8.jpg', 27);
+    addMemeToDB('9.jpg', 28);
+    addMemeToDB('10.jpg', 29);
 }
 
-function searchHistory(res, filename, callback) {
+function searchHistory(res, req, filename, callback) {
     let zapytanie = 'SELECT date, price FROM meme WHERE url = "' + filename + '" ORDER BY date DESC;';
     db.all(zapytanie, [], (err, rows) => {
         if (err) throw (err);
 
         let history = [];
-        for(let {date, price} of rows) {
+        for (let {date, price} of rows) {
             let o = {date: date, price: price};
             history.push(o);
         }
-        callback(res, filename, history);
+        callback(res, req, filename, history);
     });
 }
 
-function addNewPrice(res, filename, newPrice, callback) {
+function addNewPrice(res, req, filename, newPrice) {
     db.run('UPDATE meme SET actual = 0 WHERE url = "' + filename + '";');
     let date: string = new Date().toISOString().slice(0, 19).replace('T', ' ');
     db.run('INSERT INTO meme (url, price, date, actual) VALUES ("' + filename + '", ' + newPrice + ', "' + date + '", 1);');
@@ -51,18 +48,17 @@ function addNewPrice(res, filename, newPrice, callback) {
         if (err) throw (err);
 
         let history = [];
-        for(let {date, price} of rows) {
+        for (let {date, price} of rows) {
             let o = {date: date, price: price};
             history.push(o);
         }
-        callback(res, filename, history);
     });
 }
 
-function openPageHistory(res, filename: string, history) {
+function openPageHistory(res, req, filename: string, history) {
     console.log("open " + filename);
     console.log(history);
-    res.render('history', {title: 'meme price history', url: filename, history: history});
+    res.render('history', {title: 'meme price history', url: filename, history: history, token: req.csrfToken()});
 }
 
 function chooseTheMostExpensive3(res, callback) {
@@ -72,7 +68,7 @@ function chooseTheMostExpensive3(res, callback) {
 
         let selected = [];
         let i = 0;
-        for(let {url} of rows) {
+        for (let {url} of rows) {
             let o = {url: url};
             selected.push(o);
             i++;
@@ -86,73 +82,81 @@ function chooseTheMostExpensive3(res, callback) {
 
 function openPageMain(res, selected) {
     console.log(selected);
-    res.render('index', { title: 'meme market', message: 'I\'m not good in memes, so I used random pictures, sorry', memes: selected });
+    res.render('index', {
+        title: 'meme market',
+        message: 'I\'m not good in memes, so I used random pictures, sorry',
+        memes: selected
+    });
 }
 
-let express = require('express');
-let app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const csurf = require('csurf');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const csrfMiddleware = csurf({
+    cookie: true
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
 app.set('view engine', 'pug');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     chooseTheMostExpensive3(res, openPageMain);
 });
 
-app.get('/assets/:image', function(req, res) {
+function openImage(req, res, justOpen: boolean) {
     let fd;
-    let filename = './assets/' + req.params.image;
-    console.log(filename);
-    open(filename, 'a').then((_fd) => {
+    let path = './assets/' + req.params.image;
+    let filename = req.params.image;
+    open(path, 'a').then((_fd) => {
         fd = _fd;
-        fs.readFile(filename, (err, data) => {
+        fs.readFile(path, (err, data) => {
             if (err) {
                 res.writeHead(404);
                 res.write(err);
                 res.end();
-            } else {
+            } else if (justOpen) {
                 res.write(data);
                 res.end();
+            } else {
+                searchHistory(res, req, filename, openPageHistory);
             }
         });
     }).then(() => close(fd)).catch((reason) => {
         console.log('Błąd był straszliwy!', reason);
     });
+}
+
+app.get('/assets/:image', function (req, res) {
+    openImage(req, res, true);
 });
 
+app.get('/history/:image', function (req, res) {
+    openImage(req, res, false);
+});
 
-app.get('/views/assets/:image', function(req, res) {
-    let fd;
-    let filename = './assets/' + req.params.image;
-    open(filename, 'a').then((_fd) => {
-        fd = _fd;
-        fs.readFile(filename, (err, data) => {
-            if (err) {
-                res.writeHead(404);
-                res.write(err);
-                res.end();
-            } else if (req.url.length < ("/views/assets/?changePrice=1&submit=change").length + req.params.image.length) {
-                console.log("hej " + filename + " " + req.url);
-                searchHistory(res, filename, openPageHistory);
-            } else {
-                let help = req.url.split('/');
-                let help2 = (help[help.length -1]).split('=');
-                let help3 = (help2[1]).split('&');
-                let newPrice = parseInt(help3[0]);
-                console.log(newPrice);
-                if (isNaN(newPrice)) {
-                    console.log("Błędna wartość nowej ceny!");
-                    searchHistory(res, filename, openPageHistory);
-                } else {
-                    addNewPrice(res, filename, newPrice, openPageHistory);
-                }
-            }
-        });
-    }).then(() => close(fd)).catch((reason) => {
-        console.log('Błąd był straszliwy!', reason);
-    });
+app.post('/history/:image', (req, res) => {
+    console.log(`Message received: ${req.body.changePrice}`);
+    console.log(`CSRF token used: ${req.body._csrf}`);
+
+    if (isNaN(req.body.changePrice)) {
+        console.log("Błędne wartość ceny.");
+    } else {
+        addNewPrice(res, req, req.params.image, req.body.changePrice);
+    }
+
+    openImage(req, res, false);
 });
 
 //add10Memes();
-
-app.listen(8080, function() {
-    console.log('App listening on port 8080!');
+const PORT = 3000;
+app.listen(PORT, function () {
+    console.log(`Listening on http://localhost:${PORT}`);
 });
